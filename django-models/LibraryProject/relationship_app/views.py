d33bec42602a96ee6626004d17a9f69b92e09387
hammad-ibrahim <95659983+hammad-ibrahim@users.noreply.github.com>
2026-01-18 02:09:20 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
 
 from .forms import RegisterForm
 from .models import Library, Book
