2c6e7fa410f7073afb5a8390bcebb5c26a6ee848
hammad-ibrahim <95659983+hammad-ibrahim@users.noreply.github.com>
2026-01-18 03:11:18 +0200

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from .views import list_books, add_book, edit_book, delete_book
+from django.urls import path
+from .views import (
+    list_books,
+    add_book,
+    edit_book,
+    delete_book
+)
 
 urlpatterns = [
-    path('books/', list_books, name='books_list'),
-    path('books/add/', add_book, name='add_book'),
-    path('books/edit/<int:book_id>/', edit_book, name='edit_book'),
-    path('books/delete/<int:book_id>/', delete_book, name='delete_book'),
+    path('books/', list_books, name='books_list'),  # public list of books
+    path('books/add/', add_book, name='add_book'),  # secured add book
+    path('books/edit/<int:book_id>/', edit_book, name='edit_book'),  # secured edit book
+    path('books/delete/<int:book_id>/', delete_book, name='delete_book'),  # secured delete book
 ]
