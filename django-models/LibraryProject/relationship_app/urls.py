7474996913e4c526494e9b978f9d097436506d41
hammad-ibrahim <95659983+hammad-ibrahim@users.noreply.github.com>
2026-01-18 02:43:31 +0200

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,17 +1,21 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
-from . import views, admin_view
+from .views import (
+    list_books, LibraryDetailView,
+    register_view, login_view, logout_view,
+    admin_view, librarian_view, member_view
+)
 
 urlpatterns = [
-    # Books & Library
-    path('books/', views.list_books, name='books_list'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    # Authentication
-    path('login/',LoginView.as_view(template_name='relationship_app/login.html'),name='login'),
-    path('logout/',LogoutView.as_view(template_name='relationship_app/logout.html'),name='logout'),
-    path('register/', views.register_view, name='register'),
-    path('admin-role/', views.admin_view, name='admin_view'),
-    path('librarian-role/', views.librarian_view, name='librarian_view'),
-    path('member-role/', views.member_view, name='member_view'),
-        path('admin-role/', admin_view, name='admin_view'),
+    path('books/', list_books, name='books_list'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+
+    # Authentication URLs
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+
+    # Role-Based URLs
+    path('admin-role/', admin_view, name='admin_view'),
+    path('librarian-role/', librarian_view, name='librarian_view'),
+    path('member-role/', member_view, name='member_view'),
 ]
