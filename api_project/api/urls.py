a70f04ff3554502197a3ac3f1ed4590c1bae431c
hammad-Ibrahim <hammad.ibrahim21994@gmail.com>
2026-02-01 22:27:52 +0200

    feat: implement CRUD operations for Book model with ViewSet & Router
    - Added BookViewSet using ModelViewSet for full CRUD
    - Registered BookViewSet in DefaultRouter
    - Updated api/urls.py to include router URLs for CRUD endpoints
    - Kept BookList view (ListAPIView) for reference
    - Verified GET, POST, PUT, PATCH, DELETE endpoints with DRF

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,19 @@
-from django.urls import path
-from .views import BookList
-from ..api_project.urls import urlpatterns
+from os.path import basename
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookList, BookViewSet
+
+
+# create router and register the viewset
+router  =DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
+
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
+
+    # Include router URLS for BOokViewSet (full CRUD)
+    path('', include(router.urls)),
+
 ]
