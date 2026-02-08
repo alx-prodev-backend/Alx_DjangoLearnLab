06b2969e9cc92ec0fd8b0129bfe28001cc755ff7
hammad-Ibrahim <hammad.ibrahim21994@gmail.com>
2026-02-09 01:47:28 +0200

    feat: add CRUD views for Book using generic views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,8 +4,58 @@ from .models import   Book
 from .serializers import  BookSerializer
 
 # Create your views here.
+# starter way
+from rest_framework.generics import (
+    ListAPIView,
+    RetrieveAPIView,
+    CreateAPIView,
+    UpdateAPIView,
+    DestroyAPIView
+)
+from .models import Book
+from .serializers import BookSerializer
 
 
+class ListView(ListAPIView):
+    """
+    Retrieve all books.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class DetailView(RetrieveAPIView):
+    """
+    Retrieve a single book by ID.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class CreateView(CreateAPIView):
+    """
+    Create a new book.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class UpdateView(UpdateAPIView):
+    """
+    Update an existing book.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class DeleteView(DestroyAPIView):
+    """
+    Delete a book.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# ِAdvanced way
 class BookViewSet(ModelViewSet):
     """
     ViewSet for handling CRUD operations on  Book model.
