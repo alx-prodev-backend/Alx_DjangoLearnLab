980e37dde56134eaa0b7696744f9183e93506b6c
hammad-ibrahim <95659983+hammad-ibrahim@users.noreply.github.com>
2026-02-13 06:11:02 +0200

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,11 +1,8 @@
-from django.shortcuts import render
-# blog/views.py
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth.mixins import  LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.decorators import login_required
-from .forms import CustomUserCreationForm, UserUpdateForm
-
+from django.urls import reverse_lazy # Better for success_url in CBVs
 from django.views.generic import (
     ListView,
     DetailView,
@@ -14,7 +11,9 @@ from django.views.generic import (
     DeleteView
 )
 from .models import Post
-# Existing home/posts views...
+from .forms import CustomUserCreationForm, UserUpdateForm
+
+# --- Authentication Views ---
 
 def register(request):
     if request.method == 'POST':
@@ -28,7 +27,7 @@ def register(request):
         form = CustomUserCreationForm()
     return render(request, 'blog/register.html', {'form': form})
 
-@login_required  # Decorator
+@login_required
 def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
@@ -39,49 +38,55 @@ def profile(request):
     else:
         u_form = UserUpdateForm(instance=request.user)
 
-    context = {
-        'u_form': u_form
-    }
-    return render(request, 'blog/profile.html', context)
+    return render(request, 'blog/profile.html', {'u_form': u_form})
 
-# 1. List View (Accessible to all)
+
+# --- Blog Post Management (CRUD) ---
+
 class PostListView(ListView):
     model = Post
-    template_name = 'blog/home.html'  # Use your existing home template
+    template_name = 'blog/home.html'
     context_object_name = 'posts'
-    ordering = ['-date_posted']
+    # FIXED: Matching the field name in your model choices
+    ordering = ['-published_date'] 
+    paginate_by = 5 # Professional touch: shows 5 posts per page
 
-# 2. Detail View (Accessible to all)
 class PostDetailView(DetailView):
     model = Post
+    # Django looks for blog/post_detail.html by default
 
-# 3. Create View (Login required)
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
+    # Django looks for blog/post_form.html by default
 
     def form_valid(self, form):
-        form.instance.author = self.request.user # Automatically set author
+        form.instance.author = self.request.user
+        messages.success(self.request, "Post created successfully!")
         return super().form_valid(form)
 
-# 4. Update View (Author only)
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
+        messages.success(self.request, "Post updated successfully!")
         return super().form_valid(form)
 
     def test_func(self):
         post = self.get_object()
-        return self.request.user == post.author # Security Check
+        return self.request.user == post.author
 
-# 5. Delete View (Author only)
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
-    success_url = '/' # Redirect home after deletion
+    # reverse_lazy is safer for redirects in class attributes
+    success_url = reverse_lazy('blog-home')
 
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
+    
+    def delete(self, request, *args, **kwargs):
+        messages.info(self.request, "Post deleted.")
+        return super().delete(request, *args, **kwargs)
