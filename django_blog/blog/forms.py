e73881d7f01ba78c4f3b4018a3ca12470f32a16f
hammad-ibrahim <95659983+hammad-ibrahim@users.noreply.github.com>
2026-02-13 14:06:57 +0200

    Update forms.py
    Fix : tag problem

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Comment, Post
-from taggit.forms import TagWidget # 👈 Mandatory import for the check
+from taggit.forms import TagWidget 
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField()
@@ -18,16 +18,12 @@ class UserUpdateForm(forms.ModelForm):
         model = User
         fields = ['username', 'email']
 
-# --- MANDATORY: PostForm with TagWidget ---
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
         widgets = {
-            'tags': TagWidget(attrs={
-                'class': 'form-control',
-                'placeholder': 'Enter tags separated by commas (e.g. django, python)'
-            }),
+            'tags': TagWidget(), 
         }
 
 class CommentForm(forms.ModelForm):
