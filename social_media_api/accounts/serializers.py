72e226483ba3c4070483e4e195d479267b9d02a9
hammad-ibrahim <95659983+hammad-ibrahim@users.noreply.github.com>
2026-02-22 19:02:51 +0200

    Update serializers.py
    Fixing : accounts/serializers.py doesn't contain: ["get_user_model().objects.create_user"]

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -4,25 +4,23 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
+
 User = get_user_model()
 
-
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
+        fields = ['id', 'username', 'email', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
 
     def create(self, validated_data):
-        password = validated_data.pop('password')
-        user = User(**validated_data)
-        user.set_password(password)  # 🔐 Hash password
-        user.save()
-
-        # Create token
-        Token.objects.create(user=user)
-
+        user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
         return user
 
 
