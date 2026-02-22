b5258fd85eb996688727f5185b1ec3b1f1c1f1c3
hammad-Ibrahim <hammad.ibrahim21994@gmail.com>
2026-02-22 18:56:58 +0200

    Pushing missing files

---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,51 @@
+from django.contrib.auth import get_user_model
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.models import Token
+
+from .serializers import RegisterSerializer, LoginSerializer, ProfileSerializer
+
+User = get_user_model()
+
+
+class RegisterView(APIView):
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+            token = Token.objects.get(user=user)
+
+            return Response({
+                "user": RegisterSerializer(user).data,
+                "token": token.key
+            }, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            token, _ = Token.objects.get_or_create(user=user)
+
+            return Response({
+                "token": token.key
+            }, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = ProfileSerializer(request.user)
+        return Response(serializer.data)
